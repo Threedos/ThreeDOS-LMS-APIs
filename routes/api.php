af2a3f4f6e477b3d2e0b9f76eecb2b296f29c106
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-03 14:14:33 +0200

    x

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,10 @@ use App\Http\Controllers\api\CouncilController;
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
 // Route::post('register', [AuthController::class, 'register']);
-Route::apiResource('councils', CouncilController::class, ['only' => ['index', 'show']]);
 
 Route::middleware(['auth:api', 'throttle:api'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::apiResource('councils', CouncilController::class, ['only' => ['store', 'update', 'destroy']]);
+    Route::apiResource('councils', CouncilController::class);
     Route::apiResource('users', UserController::class);
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('tasks', TaskController::class);
