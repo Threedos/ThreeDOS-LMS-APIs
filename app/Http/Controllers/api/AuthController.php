439c7c8b636b7d98220453e87c2651a13d8ec62e
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-08 20:00:03 +0200

    Adding Role Name

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -40,7 +40,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'expires_in' => JWTAuth::factory()->getTTL() * 60,
-            'role' => $user->role
+            'role' => $user->role->name
         ]);
     }
 
