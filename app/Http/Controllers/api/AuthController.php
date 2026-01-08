7f4e87da153493fa3988a8851678f06b98a3b7e3
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-08 19:56:32 +0200

    Role

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -39,7 +39,8 @@ class AuthController extends Controller
 
         return response()->json([
             'access_token' => $token,
-            'expires_in' => JWTAuth::factory()->getTTL() * 60
+            'expires_in' => JWTAuth::factory()->getTTL() * 60,
+            'role' => $user->role
         ]);
     }
 
