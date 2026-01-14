ce64143d656e373f4a1285bd21f8e89e7e0c8273
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-15 01:43:19 +0200

    v

--- app/Http/Middleware/RateLimiting.php
+++ app/Http/Middleware/RateLimiting.php
@@ -22,7 +22,7 @@ class RateLimiting
         $limits = [
             'minute' => ['limit' => $user->rate_limit ?? 60, 'window' => 60],
             'hour' => ['limit' => $user->hourly_limit ?? 200, 'window' => 3600],
-            'day' => ['limit' => $user->daily_limit ?? 3000, 'window' => 86400],
+            'day' => ['limit' => $user->daily_limit ?? 300, 'window' => 86400],
         ];
 
         foreach ($limits as $tier => $config) {
