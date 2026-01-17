c69a866edcab8a0cab2c14bb280c65e6f66e9953
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-17 23:09:06 +0200

    CORS UPDATED

--- config/cors.php
+++ config/cors.php
@@ -7,8 +7,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
     ],
 
     'allowed_origins_patterns' => [],
