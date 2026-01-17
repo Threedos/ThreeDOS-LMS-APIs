e729a68bc706c17935da685be3997f0f4eea5de4
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-17 16:41:07 +0200

    saddds

--- database/migrations/2026_01_17_143946_edit_attendance_table.php
+++ database/migrations/2026_01_17_143946_edit_attendance_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('attendances  ', function (Blueprint $table) {
+        Schema::table('attendances', function (Blueprint $table) {
             //
         });
     }
