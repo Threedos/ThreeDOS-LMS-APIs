698ca3bdbc5f42cb143bb28078e853b29ae0f30d
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-18 02:53:14 +0200

    test

--- config/mail.php
+++ config/mail.php
@@ -6,29 +6,37 @@ return [
     |--------------------------------------------------------------------------
     | Default Mailer
     |--------------------------------------------------------------------------
+    |
+    | This option controls the default mailer that is used to send all email
+    | messages unless another mailer is explicitly specified when sending
+    | the message. You may set this to "smtp", "log", "array", etc.
+    |
     */
-    'default' => env('MAIL_MAILER', 'log'),
+
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
     |--------------------------------------------------------------------------
+    |
+    | Here you may configure all of the mailers used by your application plus
+    | their respective settings. Laravel supports SMTP, Mailtrap, Log, Array, etc.
+    |
     */
-    'mailers' => [
-        'mailersend' => [
-            'transport' => 'mailersend',
-        ],
-        'smtp' => [
-        'transport' => 'smtp',
-        'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-        'port' => env('MAIL_PORT', 587),
-        'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-        'username' => env('MAIL_USERNAME'),
-        'password' => env('MAIL_PASSWORD'),
-        'timeout' => 60,
-        'auth_mode' => null,
-        ],
 
+    'mailers' => [
+
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'sandbox.smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
+            'encryption' => env('MAIL_ENCRYPTION', null),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
+        ],
 
         'log' => [
             'transport' => 'log',
@@ -39,25 +47,21 @@ return [
             'transport' => 'array',
         ],
 
-        'sendmail' => [
-            'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
-        ],
-
-        'failover' => [
-            'transport' => 'failover',
-            'mailers' => ['smtp', 'log'],
-        ],
     ],
 
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
     |--------------------------------------------------------------------------
+    |
+    | All emails sent by the application will use this address unless overridden
+    | in a mailable.
+    |
     */
+
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', env('APP_NAME')),
+        'address' => env('MAIL_FROM_ADDRESS', 'backendhead.threedos26@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'ThreeDOS'),
     ],
 
 ];
