6cfd25b8bdde3e1a959703e4839ff5d28a901eb3
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-20 00:13:45 +0200

    edits

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -16,7 +16,7 @@ class TaskResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'title' => $this->title,
             'description' => $this->description,
             'status' => $this->status,
             'created_at' => $this->created_at,
