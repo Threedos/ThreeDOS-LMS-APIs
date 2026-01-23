dcab273115f155767eec38449157e4ffa56b50a5
Mohamed-Tarek <Mohamed.Tarek.22230180@commerce.helwan.edu.eg>
2026-01-23 15:11:45 +0200

    Edits

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
     /** @use HasFactory<\Database\Factories\AttendanceFactory> */
-    use HasFactory;
+    use HasFactory,HasUuids;
 
     protected $table = 'attendances';
     protected $keyType = 'string';
