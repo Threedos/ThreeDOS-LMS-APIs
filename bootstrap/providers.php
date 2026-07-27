67c2c127e7aa39da20131b7caa8ab6d0bf612117
Mohamed Tarek <mohamedtarekbadr047@gmail.com>
2026-07-27 14:27:06 +0300

    feat: install and configure Laravel Telescope for debugging and monitoring

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
